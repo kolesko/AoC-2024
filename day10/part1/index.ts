372db6dfde38a3cd2f07d11004aa6254b978e450
kolesko <kolesarmatejko@gmail.com>
2024-12-10 19:03:53 +0100

    refactor day10

--- day10/part1/index.ts
+++ day10/part1/index.ts
@@ -3,106 +3,67 @@ import { readFileSync } from "fs";
 type Point = { x: number; y: number };
 type PointWithVisited = { point: Point; visited: Set<string> };
 
+const input = readFileSync(process.env.INPUT_PATH!, "utf-8");
+const directions: Point[] = [
+  { x: 0, y: 1 }, // down
+  { x: 1, y: 0 }, // right
+  { x: 0, y: -1 }, // up
+  { x: -1, y: 0 }, // left
+];
+const grid: number[][] = [];
+let w = 0;
+let h = 0;
+const startingPoints: Point[] = [];
+
+function parseInput() {
+  for (const line of input.split("\n")) {
+    const row: number[] = [];
+    w = 0;
+    for (const char of line) {
+      if (char === "0") {
+        startingPoints.push({ x: w, y: h });
+      }
+      row.push(char === "." ? 99 : parseInt(char));
+      w++;
+    }
+    grid.push(row);
+    h++;
+  }
+}
+
+function findPath(cp: Point, seen: Set<string> = new Set()): number {
+  let result = 0;
+  const currentValue = grid[cp.y][cp.x];
+  if (seen.has(`${cp.x},${cp.y}`)) {
+    return 0;
+  }
+  seen.add(`${cp.x},${cp.y}`);
+  if (currentValue === 9) {
+    return 1;
+  }
+  for (const dir of directions) {
+    const np = { x: cp.x + dir.x, y: cp.y + dir.y };
+    if (np.x < 0 || np.y < 0 || np.x >= w || np.y >= h) {
+      continue;
+    }
+    const nextValue = grid[np.y][np.x];
+    if (nextValue - currentValue === 1) {
+      result += findPath(np, seen);
+    }
+  }
+  return result;
+}
+
+function solve() {
+  let score = 0;
+  startingPoints.forEach((startingPoint) => {
+    const x = findPath(startingPoint);
+    score += x;
+  });
+  return score;
+}
+
 export default function main() {
-  const input = readFileSync(process.env.INPUT_PATH!, "utf-8");
-  const grid: number[][] = [];
-  const startingPoints: Point[] = [];
-  const directions: Point[] = [
-    { x: 0, y: 1 }, // down
-    { x: 1, y: 0 }, // right
-    { x: 0, y: -1 }, // up
-    { x: -1, y: 0 }, // left
-  ];
-  let w = 0;
-  let h = 0;
-
-  function parseInput() {
-    for (const line of input.split("\n")) {
-      const row: number[] = [];
-      w = 0;
-      for (const char of line) {
-        if (char === "0") {
-          startingPoints.push({ x: w, y: h });
-        }
-        row.push(char === "." ? 99 : parseInt(char));
-        w++;
-      }
-      grid.push(row);
-      h++;
-    }
-  }
-
-  function findPath(
-    startingPoint: Point,
-    direction: Point,
-    prevValue: number,
-    visited: PointWithVisited[],
-    heads: Set<string>
-  ) {
-    const vInd = visited.findIndex(
-      (v) => v.point.x === startingPoint.x && v.point.y === startingPoint.y
-    );
-    if (vInd === -1) {
-      visited.push({ point: startingPoint, visited: new Set() });
-    }
-    const newPoint = {
-      x: startingPoint.x + direction.x,
-      y: startingPoint.y + direction.y,
-    };
-    if (
-      newPoint.x < 0 ||
-      newPoint.x >= w ||
-      newPoint.y < 0 ||
-      newPoint.y >= h
-    ) {
-      return false;
-    }
-    const addToVisited = `${newPoint.x},${newPoint.y}`;
-    const visitedSet = visited.find(
-      (v) => v.point.x === startingPoint.x && v.point.y === startingPoint.y
-    )?.visited;
-    if (visitedSet?.has(addToVisited)) {
-      return false;
-    }
-    if (visitedSet) {
-      visitedSet.add(addToVisited);
-    }
-    if (prevValue === 8 && grid[newPoint.y][newPoint.x] === 9) {
-      heads.add(`${newPoint.x},${newPoint.y}`);
-      return true;
-    }
-    if (grid[newPoint.y][newPoint.x] - prevValue !== 1) {
-      return false;
-    }
-    if (grid[newPoint.y][newPoint.x] - prevValue === 1) {
-      for (const newDir of directions.filter(
-        (d) => !(d.x === -direction.x && d.y === -direction.y)
-      )) {
-        findPath(
-          newPoint,
-          newDir,
-          grid[newPoint.y][newPoint.x],
-          visited,
-          heads
-        );
-      }
-    }
-    return false;
-  }
-
-  function solve() {
-    let score = 0;
-    startingPoints.forEach((startingPoint, i) => {
-      const visited: PointWithVisited[] = [];
-      let result: Set<string> = new Set();
-      for (const direction of directions) {
-        findPath(startingPoint, direction, 0, visited, result);
-      }
-      score += result.size;
-    });
-    return score;
-  }
-
   parseInput();
 
   const count = solve();
--- day10/part2/index.ts
+++ day10/part2/index.ts
@@ -3,82 +3,63 @@ import { readFileSync } from "fs";
 type Point = { x: number; y: number };
 type PointWithVisited = { point: Point; visited: Set<string> };
 
+const input = readFileSync(process.env.INPUT_PATH!, "utf-8");
+const directions: Point[] = [
+  { x: 0, y: 1 }, // down
+  { x: 1, y: 0 }, // right
+  { x: 0, y: -1 }, // up
+  { x: -1, y: 0 }, // left
+];
+const grid: number[][] = [];
+let w = 0;
+let h = 0;
+const startingPoints: Point[] = [];
+
+function parseInput() {
+  for (const line of input.split("\n")) {
+    const row: number[] = [];
+    w = 0;
+    for (const char of line) {
+      if (char === "0") {
+        startingPoints.push({ x: w, y: h });
+      }
+      row.push(char === "." ? 99 : parseInt(char));
+      w++;
+    }
+    grid.push(row);
+    h++;
+  }
+}
+
+function findPath(cp: Point): number {
+  let result = 0;
+  const currentValue = grid[cp.y][cp.x];
+  if (currentValue === 9) {
+    return 1;
+  }
+  for (const dir of directions) {
+    const np = { x: cp.x + dir.x, y: cp.y + dir.y };
+    if (np.x < 0 || np.y < 0 || np.x >= w || np.y >= h) {
+      continue;
+    }
+    const nextValue = grid[np.y][np.x];
+    if (nextValue - currentValue === 1) {
+      result += findPath(np);
+    }
+  }
+  return result;
+}
+
+function solve() {
+  let score = 0;
+  startingPoints.forEach((startingPoint) => {
+    const x = findPath(startingPoint);
+    score += x;
+  });
+  return score;
+}
+
 export default function main() {
-  const input = readFileSync(process.env.INPUT_PATH!, "utf-8");
-  const grid: number[][] = [];
-  const startingPoints: Point[] = [];
-  const directions: Point[] = [
-    { x: 0, y: 1 }, // down
-    { x: 1, y: 0 }, // right
-    { x: 0, y: -1 }, // up
-    { x: -1, y: 0 }, // left
-  ];
-  let w = 0;
-  let h = 0;
-
-  function parseInput() {
-    for (const line of input.split("\n")) {
-      const row: number[] = [];
-      w = 0;
-      for (const char of line) {
-        if (char === "0") {
-          startingPoints.push({ x: w, y: h });
-        }
-        row.push(char === "." ? 99 : parseInt(char));
-        w++;
-      }
-      grid.push(row);
-      h++;
-    }
-  }
-
-  function findPath(
-    startingPoint: Point,
-    direction: Point,
-    prevValue: number,
-    heads: string[]
-  ) {
-    const newPoint = {
-      x: startingPoint.x + direction.x,
-      y: startingPoint.y + direction.y,
-    };
-    if (
-      newPoint.x < 0 ||
-      newPoint.x >= w ||
-      newPoint.y < 0 ||
-      newPoint.y >= h
-    ) {
-      return false;
-    }
-    if (prevValue === 8 && grid[newPoint.y][newPoint.x] === 9) {
-      heads.push(`${newPoint.x},${newPoint.y}`);
-      return true;
-    }
-    if (grid[newPoint.y][newPoint.x] - prevValue !== 1) {
-      return false;
-    }
-    if (grid[newPoint.y][newPoint.x] - prevValue === 1) {
-      for (const newDir of directions.filter(
-        (d) => !(d.x === -direction.x && d.y === -direction.y)
-      )) {
-        findPath(newPoint, newDir, grid[newPoint.y][newPoint.x], heads);
-      }
-    }
-    return false;
-  }
-
-  function solve() {
-    let rating = 0;
-    startingPoints.forEach((startingPoint, i) => {
-      let result: string[] = [];
-      for (const direction of directions) {
-        findPath(startingPoint, direction, 0, result);
-      }
-      rating += result.length;
-    });
-    return rating;
-  }
-
   parseInput();
 
   const count = solve();
