6287b19f97125db0d5b7870aeb1d329831783421
kolesko <kolesarmatejko@gmail.com>
2024-12-11 12:09:24 +0100

    day11

--- day11/part1/index.ts
+++ day11/part1/index.ts
@@ -1,11 +1,43 @@
 import { readFileSync } from "fs";
+const input = readFileSync(process.env.INPUT_PATH!, "utf-8");
+let numbers: string[] = [];
+
+function parseInput() {
+  const split = input.split(" ");
+  for (const number of split) {
+    numbers.push(number);
+  }
+}
+
+function blink(l: number) {
+  const newNumbers = numbers.slice();
+  let ind = 0;
+  for (let i = 0; i < l; i++) {
+    const number = numbers[i];
+    if (number === "0") {
+      newNumbers[ind] = "1";
+      ind++;
+    } else if (number.length % 2 === 0) {
+      const half = Math.floor(number.length / 2);
+      const left = parseInt(number.slice(0, half)).toString();
+      const right = parseInt(number.slice(half)).toString();
+      newNumbers.splice(ind, 1, left, right);
+      ind += 2;
+    } else {
+      newNumbers[ind] = (parseInt(number) * 2024).toString();
+      ind++;
+    }
+  }
+  numbers = newNumbers;
+}
 
 export default function main() {
-  const input = readFileSync(process.env.INPUT_PATH!, "utf-8");
-
-  function parseInput() {}
-
   parseInput();
+
+  for (let i = 0; i < 25; i++) {
+    blink(numbers.length);
+  }
+  console.log(`stones: ${numbers.length}`);
 }
 
 main();
--- day11/part2/index.ts
+++ day11/part2/index.ts
@@ -1,11 +1,47 @@
 import { readFileSync } from "fs";
+const input = readFileSync(process.env.INPUT_PATH!, "utf-8");
+const numbers: string[] = [];
+const stepsMap = new Map<string, number>();
+
+function parseInput() {
+  const split = input.split(" ");
+  for (const number of split) {
+    numbers.push(number);
+  }
+}
+function countSteps(number: number, step: number) {
+  if (step === 0) {
+    return 1;
+  }
+  if (!stepsMap.has(`${number},${step}`)) {
+    let result = 0;
+    if (number === 0) {
+      result = countSteps(1, step - 1);
+    } else if (number.toString().length % 2 === 0) {
+      const strNum = number.toString();
+      const half = Math.floor(strNum.length / 2);
+      const left = parseInt(strNum.slice(0, half));
+      const right = parseInt(strNum.slice(half));
+      result += countSteps(left, step - 1);
+      result += countSteps(right, step - 1);
+    } else {
+      result = countSteps(number * 2024, step - 1);
+    }
+
+    stepsMap.set(`${number},${step}`, result);
+  }
+  return stepsMap.get(`${number},${step}`)!;
+}
 
 export default function main() {
-  const input = readFileSync(process.env.INPUT_PATH!, "utf-8");
-
-  function parseInput() {}
-
   parseInput();
+
+  let steps = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    const number = parseInt(numbers[i]);
+    steps += countSteps(number, 75);
+  }
+  console.log(`stones: ${steps}`);
 }
 
 main();
